51a154abe35cd391dc7a6fc2e9f2012cf7c081c7
Olavo Nobre <89690397+olavonobre@users.noreply.github.com>
2023-05-29 22:19:18 +0100

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -4,4 +4,4 @@ num2 = int(input("digite o segundo número: "))
 
 resultado = num1 + num2 
 
-print('O resultado da soma entre', {}, 'e', {} 'é', resultado .format(num1, num2))
+print ('O resultado da soma entre {} e {} é {}'.format(num1, num2, resultado))
